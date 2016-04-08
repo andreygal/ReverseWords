555842081967b183372ba992df05de41bb5ff4d1
Andrey Galper <andreygalper@gmail.com>
2016-04-08 13:48:49 -0400

    works with GCJ

--- ConsoleApplication4/Source.cpp
+++ ConsoleApplication4/Source.cpp
@@ -5,27 +5,29 @@
 #include <list>
 using namespace std;
 
-int main(int arc, char *argv[]) {
+int main(int argc, char *argv[]) {
 	ifstream input_stream(argv[1]);
 	if (!input_stream)
 		cout << "Error: could not open " << argv[1] << endl; 
+	ofstream output_stream(argv[2]);
+	if (!output_stream)
+		cout << "Error: could not open " << argv[2] << endl; 
 	string word = "";
 	string line = ""; 
 	list<string> word_list; 
 	int casenum{ 1 };
-	
+	getline(input_stream, word);
 	while (getline(input_stream, line)) {
 		istringstream is { line };
 		while (is >> word) {
 			word_list.emplace_front(word);
 		}
-		cout << "Case #" << casenum << ": "; 
+		output_stream << "Case #" << casenum << ": "; 
 		for (auto word : word_list)
-			cout << word << " ";
-		cout << endl;
+			output_stream << word << " ";
+		output_stream << endl;
 		word_list.clear(); 
 		casenum++; 
 	}
-	system("pause");
 	return 0; 
 }
